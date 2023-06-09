60a4d92463a7591f6a20f690e56ea57ed17d6952
emanuelemanzoni <63074555+emanuelemanzoni@users.noreply.github.com>
2023-06-10 01:02:48 +0200

    Update shutterspeedtester.ino

--- arduino/shutterspeedtester/shutterspeedtester.ino
+++ arduino/shutterspeedtester/shutterspeedtester.ino
@@ -1,15 +1,6 @@
-#include <Adafruit_SSD1306.h>
-
-// OLED settings
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3c for 128x64 OLED
-
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // shutter speed tester settings
-#define RECEIVER_PIN A6  // pin to read the light sensor output.
+#define RECEIVER_PIN A1  // pin to read the light sensor output.
 #define TIMEOUT 8000000  // timeout value for analogPulsIn().
 #define THRESHOLD 200  // analogPlusIn() thinks HI, if the light sensor value is higher than this value.
 #define MIN_DURATION 500 // usecs. ignore the duration if it is lower than this value.
@@ -59,18 +50,13 @@ unsigned long analogPulseIn(uint8_t pin, uint8_t state, unsigned long timeoutInM
 
 
 void displayInitialScreen() {
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,3);
-
-  display.println("Shutter Speed Tester");
-  display.display();
+  Serial.println("Shutter Speed Tester");
 }
 
 void displayDuration(unsigned long duration) {
   double msecs = duration / 1000.0;
   String text = String("Time: ") + msecs + String(" msecs");
-  display.println(text);
+  //display.println(text);
   Serial.println(text);
 }
 
@@ -83,27 +69,20 @@ void displaySpeed(unsigned long duration) {
     double speeed = 1000000.0 / duration;
     text = String("Speed: 1/") + speeed + String(" secs");
   }
-  display.println(text);
+  //display.println(text);
   Serial.println(text);
 }
 
 void displayResult(unsigned long duration) {
-  display.clearDisplay();
+  //display.clearDisplay();
   displayDuration(duration);
   displaySpeed(duration);
-  display.display();  
+  //display.display();  
 }
 
 void setup() {
   pinMode(RECEIVER_PIN, INPUT);
   Serial.begin(9600);
-
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println("SSD1306 allocation failed");
-    for(;;); // Don't proceed, loop forever
-  }
-
-  display.clearDisplay();
 }
 
 void loop() {
@@ -114,6 +93,5 @@ void loop() {
     displayResult(duration);
     delay(10000);
   }  
-
-  display.clearDisplay();
+Serial.println();
 }
